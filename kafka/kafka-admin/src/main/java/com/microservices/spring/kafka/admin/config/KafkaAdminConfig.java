26784c9d7e2c9c9c9d448fe9760acfd857538621
CeruleanSource <abdullah.mohamed@ryerson.ca>
2022-01-03 15:47:01 -0500

    feat(kafka-admin): added config + dependencies

---
+++ kafka/kafka-admin/src/main/java/com/microservices/spring/kafka/admin/config/KafkaAdminConfig.java
@@ -0,0 +1,26 @@
+package com.microservices.spring.kafka.admin.config;
+
+import com.microservices.spring.config.KafkaConfigData;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.Map;
+
+@EnableRetry
+@Configuration
+public class KafkaAdminConfig {
+    public KafkaAdminConfig(KafkaConfigData kafkaConfigData) {
+        this.kafkaConfigData = kafkaConfigData;
+    }
+
+    private final KafkaConfigData kafkaConfigData;
+
+    @Bean
+    public AdminClient adminClient() {
+        return AdminClient.create(Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers()));
+    }
+}
